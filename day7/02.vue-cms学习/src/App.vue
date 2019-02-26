07764ffd82501443cb40f976f47f3cb799facd89
syq1121 <syq1341037362@163.com>
2019-02-26 13:13:41 +0800

    首页和切换动画

--- day7/02.vue-cms学习/src/App.vue
+++ day7/02.vue-cms学习/src/App.vue
@@ -5,8 +5,10 @@
 
 
         <!-- 中间的 路由 router-view 区域 -->
-
- 		<router-view></router-view>
+		<transition>
+			<router-view></router-view>
+		</transition>
+ 		
 
 
         <!-- 底部 tabbar 区域 -->
@@ -41,5 +43,19 @@ export default {
 <style scoped>
 .app-container{
     padding-top: 40px;
+	overflow-x: hidden;
 }
+.v-enter{
+	opacity: 0;
+	transform: translateX(100%)
+}
+.v-leave-to{
+	opacity: 0;
+	transform: translateX(-100%);
+	position: absolute;
+}
+.v-enter-active,.v-leave-active{
+	transition: all 0.5s ease;
+}
+
 </style>
