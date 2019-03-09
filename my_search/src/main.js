6d09c53a46f37666896a83e1c675261720756838
syq1121 <syq1341037362@163.com>
2019-03-10 00:09:45 +0800

    vuex

--- my_search/src/main.js
+++ my_search/src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App),
+    store
 }).$mount('#app')
