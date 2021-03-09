3483a77e3ad005e8286c03e9dd393de0935acfdd
syq1341037362 <44106501+syq1341037362@users.noreply.github.com>
2021-03-09 09:22:24 +0800

    Delete vue-x directory

--- vue-x/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store/index.js'
-
-new Vue({
-    render: h => h(App),
-    store
-}).$mount('#app')
--- vue-x/src/store/getters.js
+++
@@ -1,6 +0,0 @@
-const getters = {
-    isEvenOrOdd(state) {
-        return state.user.count % 2 == 0 ? '偶数' : '奇数';
-    }
-}
-export default getters
